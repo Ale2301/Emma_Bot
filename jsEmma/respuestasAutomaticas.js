7230f2e7a64d863e001c5470c59ddfb5c3b7e128
Ale2301 <alejo.2001@live.com.ar>
2024-02-14 21:50:58 -0300

    AI : Off

--- jsEmma/respuestasAutomaticas.js
+++ jsEmma/respuestasAutomaticas.js
@@ -2,7 +2,7 @@ module.exports.respuestasEmma = async function (
   message,
   respuesta,
   lastChannel,
-  client,
+  client
 ) {
   //CREAR CLIENTE PARA EMMAAI
   const claveEmma = process.env["EMMA_AI_INSTANCIA"];
@@ -51,19 +51,19 @@ module.exports.respuestasEmma = async function (
     message.reply(
       respuesta.respuestasNivelPuto[
         RNG(respuesta.respuestasNivelPuto.length - 1)
-      ],
+      ]
     );
   }
   if (message.content.toLowerCase() === "dame un abrazo") {
     message.reply(
-      respuesta.respuestasAbrazo[RNG(respuesta.respuestasAbrazo.length - 1)],
+      respuesta.respuestasAbrazo[RNG(respuesta.respuestasAbrazo.length - 1)]
     );
   }
   if (RNG(3) === (0 || 1)) {
     //Respuestas a mensajes con probabilidad
     if (message.content.toLowerCase().includes("hola")) {
       message.reply(
-        respuesta.respuestasSaludo[RNG(respuesta.respuestasSaludo.length - 1)],
+        respuesta.respuestasSaludo[RNG(respuesta.respuestasSaludo.length - 1)]
       );
     }
     if (
@@ -72,9 +72,7 @@ module.exports.respuestasEmma = async function (
     ) {
       {
         message.reply(
-          respuesta.respuestasSaludo[
-            RNG(respuesta.respuestasSaludo.length - 1)
-          ],
+          respuesta.respuestasSaludo[RNG(respuesta.respuestasSaludo.length - 1)]
         );
       }
     }
@@ -86,18 +84,18 @@ module.exports.respuestasEmma = async function (
         message.reply(
           respuesta.respuestasMenciones[
             RNG(respuesta.respuestasMenciones.length - 1)
-          ],
+          ]
         );
       }
     }
     if (message.content.toLowerCase().includes("basado")) {
       message.reply("exelente argumento pero ... "), message.react("😎");
       message.reply(
-        "92.28.211.234 N: 4.818851 Y: -75.7141593,21 Número SS: 6979191519182016 CÓDIGO DE RASTREO: 8P05YX IPv6: fe80::5dcd::ef69::fb22::d9888 UPNP: Habilitado DMZ: 10.112.42.15 MAC: 5A:75:3E:7E:00 ISP: Ucom Unversal DNS: 8.8.8.8 ALT DNS: 1.1.1.8.1 DNS: SUFFIX: Dlink WAN: 100.23.10.15 TIPO WAN: Soldado Nat GATEWAY: 192.168.0.1 MASCARILLA SUBNET: 255.255.0.255 UDP PUERTOS ABIERTOS: 8080 80 TCP PUERTOS ABIERTOS: 443 RUTA DEL VENDEDOR: ERICCSON VENDEDOR DE DISPOSITIVOS: WIN31-X TIPO DE CONEXIÓN: Ethernet ICMP HOPS:{ 192.168.01 192.168.1.1 100.73.43.4 host-132.12.32.167.ucom.com host-66.120.12.111.ucom.com 36.134.67.189 216.239.78.111 sof02s32-in-f14.1e100.net TOTAL HOPS: 8",
+        "92.28.211.234 N: 4.818851 Y: -75.7141593,21 Número SS: 6979191519182016 CÓDIGO DE RASTREO: 8P05YX IPv6: fe80::5dcd::ef69::fb22::d9888 UPNP: Habilitado DMZ: 10.112.42.15 MAC: 5A:75:3E:7E:00 ISP: Ucom Unversal DNS: 8.8.8.8 ALT DNS: 1.1.1.8.1 DNS: SUFFIX: Dlink WAN: 100.23.10.15 TIPO WAN: Soldado Nat GATEWAY: 192.168.0.1 MASCARILLA SUBNET: 255.255.0.255 UDP PUERTOS ABIERTOS: 8080 80 TCP PUERTOS ABIERTOS: 443 RUTA DEL VENDEDOR: ERICCSON VENDEDOR DE DISPOSITIVOS: WIN31-X TIPO DE CONEXIÓN: Ethernet ICMP HOPS:{ 192.168.01 192.168.1.1 100.73.43.4 host-132.12.32.167.ucom.com host-66.120.12.111.ucom.com 36.134.67.189 216.239.78.111 sof02s32-in-f14.1e100.net TOTAL HOPS: 8"
       ),
         message.react("😎");
       message.reply(
-        "y para la siguiete te va peor, si seguis molestando tu credito bancario va a bajar tanto que no te van a prestar libros en la biblioteca  ",
+        "y para la siguiete te va peor, si seguis molestando tu credito bancario va a bajar tanto que no te van a prestar libros en la biblioteca  "
       ),
         message.react("😎");
     }
@@ -109,14 +107,14 @@ module.exports.respuestasEmma = async function (
       message.reply(
         respuesta.respuestasQuienTePregunto[
           RNG(respuesta.respuestasQuienTePregunto.length - 1)
-        ],
+        ]
       );
     }
     if (message.content.toLowerCase().includes("https")) {
       message.reply(
         respuesta.respuestasHipervinculo[
           RNG(respuesta.respuestasHipervinculo.length - 1)
-        ],
+        ]
       );
     }
     if (
@@ -126,7 +124,7 @@ module.exports.respuestasEmma = async function (
       message.reply(
         respuesta.respuestasSignoPregunta[
           RNG(respuesta.respuestasSignoPregunta.length - 1)
-        ],
+        ]
       );
     }
     if (
@@ -136,16 +134,16 @@ module.exports.respuestasEmma = async function (
       message.reply(
         respuesta.respuestasSignoAdmiracion[
           RNG(respuesta.respuestasSignoAdmiracion.length - 1)
-        ],
+        ]
       );
     }
     if (message.content.toLowerCase().includes("alla")) {
       message.reply(
-        respuesta.respuestasAlla[RNG(respuesta.respuestasAlla.length - 1)],
+        respuesta.respuestasAlla[RNG(respuesta.respuestasAlla.length - 1)]
       );
     }
   }
-  if (RNG(0) === 0) {
+  if (RNG(1) === 0) {
     let input = message.content;
     console.log(input);
     fetch("https://emma.cristianalvara9.repl.co/api", {
